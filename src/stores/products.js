c7fc49746af7c15c78f405a1a29501b4ebff6ed6
Andriucm <andreimihalatxe@gmail.com>
2024-12-05 20:26:29 +0100

    orderBy

--- src/stores/products.js
+++ src/stores/products.js
@@ -25,14 +25,14 @@ export const useProductsStore = defineStore("products", () => {
 			if (lastVisible.value) {
 				productQuery = query(
 					collection(db, "products"),
-					orderBy("price", "desc"), 
+					orderBy("price"), 
 					startAfter(lastVisible.value),
 					limit(9)
 				);
 			} else {
 				productQuery = query(
 					collection(db, "products"),
-					orderBy("price", "desc"), 
+					orderBy("price"), 
 					limit(9)
 				);
 			}
